2c3bdbec74e8e404aeaf9db6e66d8972b8998e1f
nkq1304 <103891057+nkq1304@users.noreply.github.com>
2023-04-21 11:07:09 +0700

    delete app

--- app/core/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "config.php"; //1
-require "Database.php";
-require "Controller.php";
-require "Model.php";
-require "App.php";
--- app/views/home.view.php
+++
@@ -1,5 +0,0 @@
-<?php $this->view('includes/header')?>
-
-    <h1>This is homepage v1</h1>
-
-<?php $this->view('includes/footer')?>
