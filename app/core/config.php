be04484387469e7955ca1caea548fe4368abdabd
nkq1304 <nkq1304@gmail.com>
2023-04-21 11:24:07 +0700

    fixed config.php file

--- app/core/config.php
+++ app/core/config.php
@@ -7,7 +7,7 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
     define('DB_USER', 'root');
     define('DB_PASS', '');
     
-    define('ROOT', 'http://localhost/MVC_test_v0/public');
+    define('ROOT', 'http://localhost/UWC2.0/public');
     define('APPROOT', dirname(dirname(__FILE__)));
     //define('VIEWSURL', 'http://localhost/assignment-ltw/app/views');
     //definr('')
