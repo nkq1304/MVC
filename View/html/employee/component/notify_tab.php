765f19c7bda12d9a725a9b0413cff57349464c57
manhcuong1502 <74718538+manhcuong1502@users.noreply.github.com>
2023-04-22 11:20:11 +0700

    Update notify_tab.php

---
+++ View/html/employee/component/notify_tab.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giao tiếp</title>
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/style1.scss">
+
+</head>
+<body class=" box-sizing-content d-flex justify-content-center">
+    <div class="container position-absolute border-1 bg-white border-dark shadow" style="height:80%">
+        <div class="row no-gutters " style="height:100%">
+            <div class="col-md-4 border-right">
+                <div class="settings-tray">
+                    <div class="row">
+                        <div class="col-2">
+                            <img class="profile-image" src="<?=ROOT?>/assets/images/avt_user.jpg" alt="Profile img">
+                        </div>
+                        <div class="col-10 h1 text-start">
+                            Cường
+                        </div>
+                    </div>
+                </div>
+                <div class="search-box">
+                    <div class="input-wrapper">
+                        <i class="material-icons">search</i>
+                        <input placeholder="Search here" type="text">
+                    </div>
+                </div>
+                <div class="overflow-y-scroll" style="height:400px">
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Trâm
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            23:22
+                        </div>
+                    </div> 
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Hoàng
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            15:32
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Hà
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Quỳnh
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Hoàng
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Minh
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Huy
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                    <hr>
+                    <div type="button" href="#" class="row bg-secondary m-0 ">
+                        <div class="col-md-2 p-0 m-2"> 
+                            <img class="profile-image " src="<?=ROOT?>/assets/images/avt_user.jpg" alt="">
+                        </div>
+                        <div class="col-md-7 h3 p-0 mt-3">
+                            Đức
+                        </div>
+                        <div class="col-md-2 p-0 mt-3">
+                            00:00
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-8 p-2"  >
+                <div class="row d-flex align-content-between" style="height:100%;width:100%">
+                    <div class="col-md-12 chat-panel overflow-y-scroll"  style="height:450px" >
+                        <div class="row" >
+                            <div class="col-md-3 " >
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 ">
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 ">
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 ">
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 ">
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 ">
+                                <div class="chat-bubble">
+                                    123 456
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-3 offset-md-9">
+                                <div class="chat-bubble text-dark">
+                                    123
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form action="" method="post">
+                                    <label for="">Tin nhắn</label>
+                                    <input type="text" name="content_s" id="content_s"   class=" border border-black m-2">
+                                    <input type="submit" value="Gửi" name="submit" style="width:100%">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
